c6e30d2613e903f6a2113006d7993262deaac609
Lewis <mojicalewis@gmail.com>
2025-07-14 04:49:06 -0400

    add script that add the ncf data to the sale invoice before is summited

---
+++ dgii_compliance/dgii_compliance/add_ncf_data.py
@@ -0,0 +1,59 @@
+
+
+def main(doc,method):
+	"""
+	Generate or assign NCF (Número de Comprobante Fiscal) for Dominican Republic fiscal invoices.
+
+	- New invoices (3 dashes): generates sequential NCF from enabled sequences
+	- Cancelled invoices (4 dashes): copies NCF from original invoice
+
+	Args:
+	doc: Sales Invoice document to process
+	"""
+	if doc.custom_tipo_de_factura== 'Valor fiscal':
+		if doc.name.count('-') == 3:
+			
+			ncf_seq_list = frappe.get_all('Secuencia NCF', filters = {'enabled': 1})
+			#parent_invoices = frappe.get_all('Sales Invoice', filters = {'status': 'Cancelled'})
+			#frappe.throw('No existen secuencias de ncf habilitadas. Contacte administrador del sistema')
+			#parent_invoices
+			#log
+			
+			#error check
+			if len(ncf_seq_list) == 0:
+				frappe.throw('No existen secuencias de ncf habilitadas. Contacte administrador del sistema')
+			if frappe.db.get_value('Secuencia NCF', ncf_seq_list[0], 'end') < frappe.db.get_value('Secuencia NCF', ncf_seq_list[0], 'next_ncf'): #si el siguiente ncf no supera el ncf final se escribe un nuevo ncf en la factura
+				frappe.throw('next_ncf es mayor que end_ncf. Contacte administrador del sistema')
+			if frappe.db.get_value('Secuencia NCF', ncf_seq_list[0], 'start') > frappe.db.get_value('Secuencia NCF', ncf_seq_list[0], 'next_ncf'): #si el siguiente ncf no supera el ncf final se escribe un nuevo ncf en la factura
+				frappe.throw('next_ncf es menor que start_ncf. Contacte administrador del sistema')
+			
+			end_ncf = frappe.db.get_value('Secuencia NCF', ncf_seq_list[0], 'end')
+			next_ncf = frappe.db.get_value('Secuencia NCF', ncf_seq_list[0], 'next_ncf')
+			start_ncf = frappe.db.get_value('Secuencia NCF', ncf_seq_list[0], 'start')
+			
+			#avisar si se estan agotando los ncf
+			no_ncf = end_ncf-start_ncf
+			if len(ncf_seq_list) < 2 and (next_ncf/no_ncf > 0.8 or end_ncf-2 < next_ncf):
+				frappe.msgprint(msg='Se están agotando los NCF',title='*Atención requerida')
+				
+			
+			if end_ncf < 1 or start_ncf < 1 or next_ncf < 1:
+				frappe.throw('uno o más valores del secuencia de ncf fuera de rango. Contacte administrador del sistema')
+			
+			
+			
+			
+			#set new ncf
+			formated_ncf = 100000000 + next_ncf
+			doc.custom_ncf= f'B0{formated_ncf}'
+			doc.custom_ncf_vencimiento = frappe.db.get_value('Secuencia NCF', ncf_seq_list[0], 'expiration_date')
+			
+			if end_ncf == next_ncf: # si se llego al final de la secuencia se deshabilita secuencia de ncf
+				frappe.db.set_value('Secuencia NCF', ncf_seq_list[0], 'enabled', 0)
+				frappe.db.set_value('Secuencia NCF', ncf_seq_list[0], 'next_ncf', -1)
+			else:
+				frappe.db.set_value('Secuencia NCF', ncf_seq_list[0], 'next_ncf', next_ncf+1)
+		
+		elif doc.name.count('-') == 4:
+			doc.custom_ncf = frappe.db.get_value('Sales Invoice', doc.name[:19],'ncf') #toma el ncf de la factura cancelada original
+		
