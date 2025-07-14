da06cab2d701676bac21462b820362659bafe9cb
Lewis <mojicalewis@gmail.com>
2025-07-14 04:49:06 -0400

    Create Secuencia NCF doctype

---
+++ dgii_compliance/dgii_compliance/doctype/secuencia_ncf/secuencia_ncf.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Lewis Mojica and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Secuencia NCF", {
+// 	refresh(frm) {
+
+// 	},
+// });
