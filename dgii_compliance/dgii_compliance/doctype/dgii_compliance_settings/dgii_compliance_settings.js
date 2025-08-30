e8c4445ab5866adc9ff67ff08b9e6d28cfd15fa1
Lewis <mojicalewis@gmail.com>
2025-08-30 19:13:46 -0400

    feat: add notification when syncing and when the sync is done

--- dgii_compliance/dgii_compliance/doctype/dgii_compliance_settings/dgii_compliance_settings.js
+++ dgii_compliance/dgii_compliance/doctype/dgii_compliance_settings/dgii_compliance_settings.js
@@ -8,12 +8,17 @@
 // });
 frappe.ui.form.on('DGII Compliance Settings', {
     sync_rnc_data: function(frm) {
+        frappe.show_alert({message: __('Syncing RNC data...'), indicator: 'blue'});
+        
         frappe.call({
             method: 'manual_sync_rnc_data',
             doc: frm.doc,
             callback: function(r) {
-                frappe.msgprint(__('RNC data sync completed'));
+                frappe.show_alert({message: __('RNC data sync completed'), indicator: 'green'});
                 frm.reload_doc();
+            },
+            error: function(r) {
+                frappe.show_alert({message: __('Sync failed'), indicator: 'red'});
             }
         });
     }
