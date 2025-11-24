fdcddde647c42c71a1d5a4a8fcd7c3b8370fe613
Lewis Mojica <mojicalewis@gmail.com>
2025-11-23 20:51:14 -0400

    feat: Add new eCF DocType

---
+++ dgii_compliance/dgii_compliance/doctype/ecf/ecf.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Lewis Mojica and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("eCF", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ dgii_compliance/dgii_compliance/doctype/ecf/ecf.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Lewis Mojica and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class eCF(Document):
+	pass
